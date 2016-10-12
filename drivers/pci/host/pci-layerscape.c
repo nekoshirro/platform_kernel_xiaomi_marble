sbourdelin/linux

PCI: layerscape: Pass device-specific struct to internal functions
Only interfaces used from outside the driver, e.g., those called by the
DesignWare core, need to accept pointers to the generic struct pcie_port.
Internal interfaces can accept pointers to the device-specific struct,
which makes them more straightforward.  No functional change intended.

Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

drivers/pci/host/pci-layerscape.c
@@ -212,11 +212,11 @@ static const struct of_device_id ls_pcie_of_match[] = {
 	{ },
 };
 
-static int __init ls_add_pcie_port(struct pcie_port *pp,
+static int __init ls_add_pcie_port(struct ls_pcie *ls_pcie,
 				   struct platform_device *pdev)
 {
+	struct pcie_port *pp = &ls->pp;
 	int ret;
-	struct ls_pcie *ls_pcie = to_ls_pcie(pp);
 
 	pp->dev = &pdev->dev;
 	pp->dbi_base = ls_pcie->dbi;
@@ -259,7 +259,7 @@ static int __init ls_pcie_probe(struct platform_device *pdev)
 	if (!ls_pcie_is_bridge(ls_pcie))
 		return -ENODEV;
 
-	ret = ls_add_pcie_port(&ls_pcie->pp, pdev);
+	ret = ls_add_pcie_port(ls_pcie, pdev);
 	if (ret < 0)
 		return ret;
 

nekoshirro/platform_kernel_xiaomi_marble

drivers/pci/host/pci-layerscape.c
@@ -213,12 +213,12 @@ static const struct of_device_id ls_pcie_of_match[] = {
 	{ },
 };
 
-static int __init ls_add_pcie_port(struct pcie_port *pp,
+static int __init ls_add_pcie_port(struct ls_pcie *pcie,
 				   struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+	struct pcie_port *pp = &pcie->pp;
 	int ret;
-	struct ls_pcie *pcie = to_ls_pcie(pp);
 
 	pp->dev = dev;
 	pp->ops = pcie->drvdata->ops;
@@ -261,7 +261,7 @@ static int __init ls_pcie_probe(struct platform_device *pdev)
 	if (!ls_pcie_is_bridge(pcie))
 		return -ENODEV;
 
-	ret = ls_add_pcie_port(&pcie->pp, pdev);
+	ret = ls_add_pcie_port(pcie, pdev);
 	if (ret < 0)
 		return ret;
 