nekoshirro/platform_kernel_xiaomi_marble

commit fc909fa68945cd54bcc941ec10f6acf7b35c28fe

power: qti_battery_charger: Add a shutdown notification
Notify charger-firmware of a shutdown event.

Change-Id: I291c9230bb7fb5df414db8f80bfb17a50c8fc7c1
Signed-off-by: Anirudh Ghayal <aghayal@codeaurora.org>

drivers/power/supply/qti_battery_charger.c
@@ -39,6 +39,7 @@
 #define BC_WLS_FW_UPDATE_STATUS_RESP	0x42
 #define BC_WLS_FW_PUSH_BUF_RESP		0x43
 #define BC_WLS_FW_GET_VERSION		0x44
+#define BC_SHUTDOWN_NOTIFY		0x47
 #define BC_GENERIC_NOTIFY		0x80
 
 /* Generic definitions */
@@ -1820,10 +1821,19 @@ static int battery_chg_parse_dt(struct battery_chg_dev *bcdev)
 static int battery_chg_ship_mode(struct notifier_block *nb, unsigned long code,
 		void *unused)
 {
+	struct battery_charger_notify_msg msg_notify = { { 0 } };
 	struct battery_charger_ship_mode_req_msg msg = { { 0 } };
 	struct battery_chg_dev *bcdev = container_of(nb, struct battery_chg_dev,
 						     reboot_notifier);
 	int rc;
+
+	msg_notify.hdr.owner = MSG_OWNER_BC;
+	msg_notify.hdr.type = MSG_TYPE_NOTIFY;
+	msg_notify.hdr.opcode = BC_SHUTDOWN_NOTIFY;
+
+	rc = battery_chg_write(bcdev, &msg_notify, sizeof(msg_notify));
+	if (rc < 0)
+		pr_err("Failed to send shutdown notification rc=%d\n", rc);
 
 	if (!bcdev->ship_mode_en)
 		return NOTIFY_DONE;

commit 9320e182b4d9ca8a7b41a39dc2af7feadb2fdf97

power: supply: qti_battery_charger: Handle BC_SHUTDOWN_NOTIFY response
During shutdown, battery charger driver sends BC_SHUTDOWN_NOTIFY
message to the charger firmware to do configuration before the
shutdown. However, the response from the charger firmware is not
handled and hence a timeout is observed in the logs. Fix it.

Fixes: fc909fa68945 ("power: qti_battery_charger: Add a shutdown notification")
Change-Id: I56f06b60bea0f1059f10684c32e9a7cf7acbdc9e
Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

drivers/power/supply/qti_battery_charger.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
  */
 
 #define pr_fmt(fmt)	"BATTERY_CHG: %s: " fmt, __func__
@@ -568,6 +568,7 @@ static void handle_message(struct battery_chg_dev *bcdev, void *data,
 
 		break;
 	case BC_SET_NOTIFY_REQ:
+	case BC_SHUTDOWN_NOTIFY:
 		/* Always ACK response for notify request */
 		ack_set = true;
 		break;