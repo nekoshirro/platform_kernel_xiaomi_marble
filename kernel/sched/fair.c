10eafdcfe106c3aa29fd3fd0fbb22712b24341f0
Templar Dev <wilfridus.it@gmail.com>
2026-04-30 16:30:49 +0000

    sched: introduce prefer_silver for GKI 5.10
    Port Oplus prefer_silver from downstream sm8250 to GKI 5.10.
    Light tasks are now preferentially placed on silver/LITTLE cores
    using upstream PELT metrics instead of WALT.
    
    - Original implementation: Oplus (ColorOS/OxygenOS)
    - Reference port: Xiaomi sm8250 (KaiFujiro/kernel_xiaomi_sm8250)
    - WALT-free adaptation using task_util / cpu_util
    - Runtime tunable via sysctl
    - Debugfs stats for hit/miss tracking
    - Configurable via CONFIG_SCHED_PREFER_SILVER
    
    Signed-off-by: Steambot12 <wilfridus.it@gmail.com>
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>
    (cherry picked from commit d95d77fd9f6012355f2220a470d215f762a28858)
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -21,6 +21,7 @@
  *  Copyright (C) 2007 Red Hat, Inc., Peter Zijlstra
  */
 #include "sched.h"
+#include <linux/prefer_silver.h>
 
 #include <trace/hooks/sched.h>
 
@@ -7093,6 +7094,13 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	}
 	rcu_read_unlock();
 
+#ifdef CONFIG_SCHED_PREFER_SILVER
+	if (prefer_silver_check_task_util(p)) {
+		int silver_cpu = find_best_silver_cpu(p);
+		if (silver_cpu >= 0)
+			return silver_cpu;
+	}
+#endif /* CONFIG_SCHED_PREFER_SILVER */
 	return new_cpu;
 }
 
